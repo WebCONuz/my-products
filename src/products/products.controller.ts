0fa9985f0a85f9b6482ee0a5298d90a3ba96af07
WebCONuz <muxammadi0799@gmail.com>
2022-12-07 16:41:53 +0500

    Swagger modulini ulash yakunlandi

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,40 +8,53 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateProductDto } from './dto/create-product.dto';
 import { QueryDto } from './dto/query.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './product.model';
 import { ProductsService } from './products.service';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   // Add Product Controller
+  @ApiOperation({ summary: 'Product yaratish' })
+  @ApiResponse({ status: 201, type: Product })
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   // Get Products By Query Strings Controller
+  @ApiOperation({ summary: "Query bo'yicha barcha productlar" })
+  @ApiResponse({ status: 200, type: [Product] })
   @Get()
   getQuery(@Query() query: QueryDto) {
     return this.productsService.getQueryProduct(query);
   }
 
   // Update Product Controller
+  @ApiOperation({ summary: "ID bo'yicha bitta product" })
+  @ApiResponse({ status: 200, type: Product })
   @Get(':id')
   getOne(@Param('id') id: number) {
     return this.productsService.getOne(id);
   }
 
   // Update Product Controller
+  @ApiOperation({ summary: 'Productni yangilash' })
+  @ApiResponse({ status: 200, type: Product })
   @Put(':id')
   update(@Param('id') id: number, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.updateProduct(updateProductDto, id);
   }
 
   // Delete Product Controller
+  @ApiOperation({ summary: "Productni o'chirish" })
+  @ApiResponse({ status: 200, type: Product })
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.productsService.deleteProduct(id);
--- src/sub_categories/sub_categories.controller.ts
+++ src/sub_categories/sub_categories.controller.ts
@@ -7,33 +7,44 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateSubCategory } from './dto/create-subcategory.dto';
 import { UpdateSubCategoryDto } from './dto/update-subcategory.dto';
+import { SubCategory } from './sub_categories.model';
 import { SubCategoriesService } from './sub_categories.service';
 
+@ApiTags('Sub Categories')
 @Controller('subcategories')
 export class SubCategoriesController {
   constructor(private readonly subCategoriesService: SubCategoriesService) {}
 
   // Create SubCategory Controller
+  @ApiOperation({ summary: 'Sub Kategoriya yaratish' })
+  @ApiResponse({ status: 201, type: SubCategory })
   @Post()
   create(@Body() createSubCategory: CreateSubCategory) {
     return this.subCategoriesService.create(createSubCategory);
   }
 
   // Get All SubCategory Controller
+  @ApiOperation({ summary: 'Barcha sub kategoriyalar' })
+  @ApiResponse({ status: 200, type: [SubCategory] })
   @Get()
   getAll() {
     return this.subCategoriesService.getAll();
   }
 
   // Get One SubCategory Controller
+  @ApiOperation({ summary: "ID bo'yicha bitta sub kategoriya" })
+  @ApiResponse({ status: 200, type: SubCategory })
   @Get(':id')
   getOne(@Param('id') id: number) {
     return this.subCategoriesService.getOne(id);
   }
 
   // Update SubCategory Controller
+  @ApiOperation({ summary: 'Sub Kategoriyani yangilash' })
+  @ApiResponse({ status: 200, type: SubCategory })
   @Put(':id')
   update(
     @Param('id') id: number,
@@ -43,6 +54,8 @@ export class SubCategoriesController {
   }
 
   // Delete SubCategory Controller
+  @ApiOperation({ summary: "Sub Kategoriyani o'chirish" })
+  @ApiResponse({ status: 200, type: SubCategory })
   @Delete(':id')
   delete(@Param('id') id: number) {
     return this.subCategoriesService.deleteCategory(id);
